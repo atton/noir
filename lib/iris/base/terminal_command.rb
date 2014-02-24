3758fdf2e8381a50b7e67ce7169467cc4d615ee4
e115763 <e115763@gmail.com>
2014-02-24 15:22:43 +0900

    Add Iris::Base::Command.execute

--- lib/iris/base/terminal_command.rb
+++ lib/iris/base/terminal_command.rb
@@ -2,5 +2,13 @@ require 'iris'
 
 module Iris::Base
   class TerminalCommand < Iris::Base::Command
+
+    def self.execute *args
+      if self == Iris::Base::TerminalCommand
+        raise 'called raw Iris::Base::TerminalCommand.execute. please call it in extended class.'
+      end
+
+    end
+
   end
 end
--- spec/iris/iris/base/terminal_command_spec.rb
+++ spec/iris/iris/base/terminal_command_spec.rb
@@ -7,4 +7,32 @@ describe 'Iris::Base::TerminalCommand' do
     expect(Iris::Base::TerminalCommand.class).to eq(Class)
   end
 
+  it 'is inherited Iris::Base::Command' do
+    expect(Iris::Base::TerminalCommand.ancestors).to include(Iris::Base::Command)
+  end
+
+  describe '.execute' do
+    it 'raise exception when not overrided' do
+      expect{Iris::Base::TerminalCommand.execute}.to raise_error(RuntimeError)
+    end
+
+    it 'not raise exception in extended class' do
+      class Fuga < Iris::Base::TerminalCommand
+      end
+
+      expect{Fuga.execute}.not_to raise_error
+    end
+
+    it 'is support varibale length arguments' do
+      class Fuga < Iris::Base::TerminalCommand
+      end
+
+      expect{Fuga.execute 0}.not_to raise_error
+      expect{Fuga.execute 0, 0}.not_to raise_error
+      expect{Fuga.execute 0, 0, 0}.not_to raise_error
+      expect{Fuga.execute 0, 0, 0, 0}.not_to raise_error
+      expect{Fuga.execute 0, 0, 0, 0, 0}.not_to raise_error
+    end
+  end
+
 end
