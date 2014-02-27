c58778c6a4988475214f1548ebf083c1393f2ece
e115763 <e115763@gmail.com>
2014-02-27 18:21:56 +0900

    Check access to SubModule on Noir

--- spec/noir/noir_spec.rb
+++ spec/noir/noir_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 require 'noir'
 
 describe Noir do
+
+  it 'is can access Noir::VERSION' do
+    expect(Noir.class_eval('Noir::VERSION.class')).to eq(String)
+  end
+
+  it 'is can access Noir::Base' do
+    expect(Noir.class_eval('Noir::Base.class')).to eq(Module)
+  end
+
+  it 'is can access Noir::Base::Command' do
+    expect(Noir.class_eval('Noir::Base::Command.class')).to eq(Class)
+  end
+
+  it 'is can access Noir::Base::TerminalCommand' do
+    expect(Noir.class_eval('Noir::Base::TerminalCommand.class')).to eq(Class)
+  end
+
 end
