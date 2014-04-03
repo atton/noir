646c8cd6226292527be82fd0c380c1120e675b67
atton <e115763@gmail.com>
2014-04-03 15:24:33 +0900

    Split stub commands into spec_helper

--- spec/noir/executer_spec.rb
+++ spec/noir/executer_spec.rb
@@ -5,34 +5,7 @@ describe 'Noir::Executer' do
 
   # define command for tests
   before do
-    stub_const('Noir::Command::Hoge'                               , Class.new(Noir::Base::Command))
-    stub_const('Noir::Command::Hoge::SubCommand'                   , Class.new(Noir::Base::Command))
-    stub_const('Noir::Command::Hoge::SubCommand::SubSubCommand'    , Class.new(Noir::Base::Command))
-    stub_const('Noir::Command::Hoge::SubCommand::SubSubNonCommand' , Class.new)
-    stub_const('Noir::Command::Hoge::SubCommandTwo'                , Class.new(Noir::Base::Command))
-    stub_const('Noir::Command::Hoge::SubNonCommand'                , Class.new)
-    stub_const('Noir::Command::Fuga'                               , Class.new(Noir::Base::TerminalCommand))
-
-=begin
-        # stub_const is this structure
-        module Noir::Command
-          class Hoge < Noir::Base::Command
-            class SubCommand < Noir::Base::Command
-              class SubSubCommand < Noir::Base::Command
-              end
-              class SubSubNonCommand
-              end
-            end
-            class SubCommandTwo < Noir::Base::Command
-            end
-
-            class SubNonCommand
-            end
-          end
-          class Fuga < Noir::Base::TerminalCommand
-          end
-        end
-=end
+    stub_commands
   end
 
   it 'is class' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,37 @@
 require 'noir'
 
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
+
+# utils
+
+def stub_commands
+  stub_const('Noir::Command::Hoge'                               , Class.new(Noir::Base::Command))
+  stub_const('Noir::Command::Hoge::SubCommand'                   , Class.new(Noir::Base::Command))
+  stub_const('Noir::Command::Hoge::SubCommand::SubSubCommand'    , Class.new(Noir::Base::Command))
+  stub_const('Noir::Command::Hoge::SubCommand::SubSubNonCommand' , Class.new)
+  stub_const('Noir::Command::Hoge::SubCommandTwo'                , Class.new(Noir::Base::Command))
+  stub_const('Noir::Command::Hoge::SubNonCommand'                , Class.new)
+  stub_const('Noir::Command::Fuga'                               , Class.new(Noir::Base::TerminalCommand))
+
+=begin
+        # stub_const is this structure
+        module Noir::Command
+          class Hoge < Noir::Base::Command
+            class SubCommand < Noir::Base::Command
+              class SubSubCommand < Noir::Base::Command
+              end
+              class SubSubNonCommand
+              end
+            end
+            class SubCommandTwo < Noir::Base::Command
+            end
+
+            class SubNonCommand
+            end
+          end
+          class Fuga < Noir::Base::TerminalCommand
+          end
+        end
+=end
+
+end
