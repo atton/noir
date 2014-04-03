ed89450c08061cadcff565d4c283b428dfeea46e
atton <e115763@gmail.com>
2014-04-03 15:29:00 +0900

    Add Noir::Command::Completion.execute

--- lib/noir/command/completion.rb
+++ lib/noir/command/completion.rb
@@ -1,7 +1,15 @@
 module Noir::Command
   class Completion < Noir::Base::TerminalCommand
 
-    @description = 'completion for shell'
+    @description = 'it is completion for shell. return command suggestion'
+
+    class << self
+
+      def execute lists
+        return ['hoge', 'fuga']
+      end
+
+    end
 
   end
 end
--- spec/noir/command/completion_spec.rb
+++ spec/noir/command/completion_spec.rb
@@ -5,4 +5,14 @@ describe 'Noir::Command::Completion' do
   it 'is Noir::Command::TerminalCommand' do
     expect(Noir::Command::Completion.class).to eq(Class)
   end
+
+  describe 'exec' do
+    before do
+      stub_commands
+    end
+
+    it 'return all command from nil' do
+        expect(Noir::Command::Completion.execute([])).to eq(['hoge', 'fuga'])
+    end
+  end
 end
