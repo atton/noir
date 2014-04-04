ad9b3ae45a7b2f3f6eb04b97e9c2110f4c8b7ad6
atton <e115763@gmail.com>
2014-04-04 10:49:54 +0900

    Support completion in zsh

--- lib/noir/command/init/zsh.rb
+++ lib/noir/command/init/zsh.rb
@@ -3,7 +3,36 @@ module Noir::Command
 
     class Zsh < Noir::Base::TerminalCommand
       @description = "init script for zsh"
-    end
 
+      # enable completion for zsh
+      # wrote this code in .zshrc
+      # if which noir  >& /dev/null; then eval "$(noir init zsh)"; fi
+
+      InitScript = %q(
+
+if [[ ! -o interactive ]]; then
+  return
+fi
+
+compctl -K _noir noir
+
+_noir() {
+  local words completions
+  read -cA words
+
+  completions="$(noir completion ${words[2,-2]})"
+
+  reply=("${(ps:\n:)completions}")
+}
+
+)
+      class << self
+
+        def execute *args
+          puts InitScript
+        end
+
+      end
+    end
   end
 end
--- spec/noir/command/init/zsh_spec.rb
+++ spec/noir/command/init/zsh_spec.rb
@@ -5,4 +5,8 @@ describe 'Noir::Command::Init::Zsh' do
   it 'is inherited Noir::Base::TerminalCommand' do
     expect(Noir::Command::Init::Zsh.superclass).to eq(Noir::Base::TerminalCommand)
   end
+
+  it 'is output in execute' do
+    expect{expect{Noir::Command::Init::Zsh.execute []}.to output.to_stdout}.to_not raise_error
+  end
 end
