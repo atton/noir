b8c2497875ac04c2df4daf9f984fe7c7d782a251
atton <e115763@gmail.com>
2014-04-09 13:33:16 +0900

    Define Noir::Command::Edit::WeeklyReport.report_name

--- lib/noir/command/edit/weekly_report.rb
+++ lib/noir/command/edit/weekly_report.rb
@@ -1,3 +1,9 @@
 class Noir::Command::Edit::WeeklyReport < Noir::Base::Command
   @description = 'edit weekly report'
+
+  class << self
+    def report_name day_of_week, week_diff=0
+      return '20140401_20140407'
+    end
+  end
 end
--- spec/noir/command/edit/weekly_report_spec.rb
+++ spec/noir/command/edit/weekly_report_spec.rb
@@ -5,4 +5,18 @@ describe 'Noir::Command::Edit::WeeklyReport' do
   it 'is inherited Noir::Base::Command' do
     expect(Noir::Command::Edit::WeeklyReport.superclass).to eq(Noir::Base::Command)
   end
+
+  describe '.report_name' do
+    before do
+      class Time
+        def self.now
+          Time.new(2014, 4, 1)  # test date is 2014/04/01
+        end
+      end
+    end
+
+    it 'is return report_name by tuesday' do
+      expect(Noir::Command::Edit::WeeklyReport.report_name :tuesday).to eq('20140401_20140407')
+    end
+  end
 end
