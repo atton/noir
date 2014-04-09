7b7f11beed49ce47bd3e20568d4f338992f7c521
atton <e115763@gmail.com>
2014-04-09 13:49:55 +0900

    Add Test for Noir::Command::Edit::WeeklyReport.report_name. has not week diff

--- spec/noir/command/edit/weekly_report_spec.rb
+++ spec/noir/command/edit/weekly_report_spec.rb
@@ -15,12 +15,39 @@ describe 'Noir::Command::Edit::WeeklyReport' do
       end
     end
 
-    it 'is return report_name by mon' do
-      expect(Noir::Command::Edit::WeeklyReport.report_name :monday).to eq('20140331_20140406')
+    it 'is raise invalid weekly separator' do
+      expect{Noir::Command::Edit::WeeklyReport.report_name :unknown}.to raise_error
     end
 
-    it 'is return report_name by tuesday' do
-      expect(Noir::Command::Edit::WeeklyReport.report_name :tuesday).to eq('20140401_20140407')
+    describe 'in no week diff' do
+      it 'is return report_name by monday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :monday).to eq('20140331_20140406')
+      end
+
+      it 'is return report_name by tuesday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :tuesday).to eq('20140401_20140407')
+      end
+
+      it 'is return report_name by wednesday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :wednesday).to eq('20140326_20140401')
+      end
+
+      it 'is return report_name by thursday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :thursday).to eq('20140327_20140402')
+      end
+
+      it 'is return report_name by friday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :friday).to eq('20140328_20140403')
+      end
+
+      it 'is return report_name by saturday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :saturday).to eq('20140329_20140404')
+      end
+
+      it 'is return report_name by sunday' do
+        expect(Noir::Command::Edit::WeeklyReport.report_name :sunday).to eq('20140330_20140405')
+      end
     end
+
   end
 end
