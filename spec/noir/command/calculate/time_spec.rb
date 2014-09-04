aca595b82743807be0f7180b5da5ceda70d3ff81
atton <e115763@gmail.com>
2014-09-04 18:46:55 +0900

    Define test helper for spec of Noir::Command::Caluclate::Time

--- spec/noir/command/calculate/time_spec.rb
+++ spec/noir/command/calculate/time_spec.rb
@@ -16,33 +16,43 @@ describe 'Noir::Command::Calculate::Time' do
       @path  = @paths.first
     end
 
+    def path_is_not_file path
+      allow(File).to receive(:exists?).with(path).and_return(false)
+    end
+
+    def path_is_file path
+      allow(File).to receive(:exists?).with(path).and_return(true)
+      allow(File).to receive(:file?).with(path).and_return(true)
+    end
+
+    def path_is_directory path
+      allow(File).to receive(:exists?).with(path).and_return(true)
+      allow(File).to receive(:file?).with(path).and_return(false)
+      allow(File).to receive(:directory?).with(path).and_return(true)
+    end
+
     it 'is raise when not found file path.' do
-      allow(File).to receive(:exists?).with(@path).and_return(false)
+      path_is_not_file @path
       expect{ Noir::Command::Calculate::Time.extract_path(@path) }.to raise_error(RuntimeError, /^File not/)
     end
 
     it 'is return raw path by exist file path' do
       @paths.each do |path|
-        allow(File).to receive(:exists?).with(@path).and_return(true)
-        allow(File).to receive(:file?).with(@path).and_return(true)
+        path_is_file @path
         expect(Noir::Command::Calculate::Time.extract_path(@path)).to eq(@path)
       end
     end
 
     it 'is return extracted path by exist directory path' do
       @paths.each do |path|
-        allow(File).to receive(:exists?).with(@path).and_return(true)
-        allow(File).to receive(:file?).with(@path).and_return(false)
-        allow(File).to receive(:directory?).with(@path).and_return(true)
+        path_is_directory @path
         allow(Dir).to receive(:entries).with(@path).and_return([@path])
         expect(Noir::Command::Calculate::Time.extract_path(@path)).to eq([@path])
       end
     end
 
     it 'is return extracted path rejected start_with by exist directory path' do
-      allow(File).to receive(:exists?).with(@path).and_return(true)
-      allow(File).to receive(:file?).with(@path).and_return(false)
-      allow(File).to receive(:directory?).with(@path).and_return(true)
+      path_is_directory @path
       allow(Dir).to  receive(:entries).with(@path).and_return(['aaa', '.aaa', 'bbb', '.bbb', 'ccc'])
       expect(Noir::Command::Calculate::Time.extract_path(@path)).to eq(["aaa", "bbb", "ccc"])
     end
