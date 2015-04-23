8cf48c096108607c2d6b74211149e34afdc333b2
atton <e115763@gmail.com>
2015-04-23 22:14:26 +0900

    Improve description of New::Gitignore

--- lib/noir/command/new/gitignore.rb
+++ lib/noir/command/new/gitignore.rb
@@ -1,11 +1,7 @@
 class Noir::Command::New::GitIgnore < Noir::Base::TerminalCommand
-  @description = 'Create new .gitignore'
-
-  GitIgnoreName = '.gitignore'
-
   module GitIgnoreTexts
     Vim = %q(
-    *.swp
+*.swp
 )
 
     TeX = %q(
@@ -21,24 +17,36 @@ class Noir::Command::New::GitIgnore < Noir::Base::TerminalCommand
 *.cpt
 )
 
+  Agda = %q(
+*.agdai
+*.*~
+)
   end
+
+  GitIgnoreName = '.gitignore'
+  SupportedKinds = GitIgnoreTexts.constants.map{|c| c.to_s.downcase}.join(' ')
+
+
 
   def self.gitignore_texts_from_kinds kinds
     kinds = kinds.map(&:downcase).map(&:to_sym)
     if kinds.empty?
-      raise 'Please input some kinds of gitignore'
+      raise "Please input some kinds of .gitignore\n supported: #{SupportedKinds}"
     end
     unless kinds.all?{|k| GitIgnoreTexts.constants.map(&:downcase).include?(k)}
       raise 'Unsupported kinds'
     end
 
-    kinds.map{|k| GitIgnoreTexts.const_get(GitIgnoreTexts.constants.find{|c| c.downcase == k})}.join("\n\n")
+    kinds.map{|k| GitIgnoreTexts.const_get(GitIgnoreTexts.constants.find{|c| c.downcase == k})}.join("\n")
   end
 
   def self.createGitIgnore text
     Noir::Command::New.createFile(GitIgnoreName, text)
   end
 
+
+
+  @description = 'Create new .gitignore. supported kinds:' + SupportedKinds
   def self.execute *args
     createGitIgnore gitignore_texts_from_kinds(args)
   end
