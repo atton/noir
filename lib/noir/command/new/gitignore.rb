15b8ed9be7aab24fb5456bd010ff18fdcf2706ef
atton <e115763@gmail.com>
2015-04-23 22:06:05 +0900

    Refactor New::Gitignore. support multi kinds

--- lib/noir/command/new/gitignore.rb
+++ lib/noir/command/new/gitignore.rb
@@ -1,13 +1,45 @@
-class Noir::Command::New::GitIgnore < Noir::Base::Command
+class Noir::Command::New::GitIgnore < Noir::Base::TerminalCommand
   @description = 'Create new .gitignore'
 
   GitIgnoreName = '.gitignore'
+
+  module GitIgnoreTexts
+    Vim = %q(
+    *.swp
+)
+
+    TeX = %q(
+*.bbl
+*.blg
+*.lof
+*.lol
+*.lot
+*.log
+*.aux
+*.dvi
+*.toc
+*.cpt
+)
+
+  end
+
+  def self.gitignore_texts_from_kinds kinds
+    kinds = kinds.map(&:downcase).map(&:to_sym)
+    if kinds.empty?
+      raise 'Please input some kinds of gitignore'
+    end
+    unless kinds.all?{|k| GitIgnoreTexts.constants.map(&:downcase).include?(k)}
+      raise 'Unsupported kinds'
+    end
+
+    kinds.map{|k| GitIgnoreTexts.const_get(GitIgnoreTexts.constants.find{|c| c.downcase == k})}.join("\n\n")
+  end
 
   def self.createGitIgnore text
     Noir::Command::New.createFile(GitIgnoreName, text)
   end
 
+  def self.execute *args
+    createGitIgnore gitignore_texts_from_kinds(args)
+  end
 end
-
-require 'noir/command/new/gitignore/vim'
-require 'noir/command/new/gitignore/tex'
--- spec/noir/command/new/gitignore_spec.rb
+++ spec/noir/command/new/gitignore_spec.rb
@@ -2,8 +2,39 @@ require 'noir'
 require 'spec_helper'
 
 describe 'Noir::Command::New::GitIgnore' do
-  it 'is Command' do
-    expect(Noir::Command::New::GitIgnore.superclass).to eq(Noir::Base::Command)
+  it 'is TerminalCommand' do
+    expect(Noir::Command::New::GitIgnore.superclass).to eq(Noir::Base::TerminalCommand)
+  end
+
+  it 'is create .gitignore' do
+    allow(Noir::Command::New).to  receive(:createFile)
+    expect(Noir::Command::New).to receive(:createFile).with('.gitignore', /.swp/)
+    Noir::Command::New::GitIgnore.execute('vim')
+  end
+
+  describe '.gitignore_texts_from_kinds' do
+    it 'is raise error when arguments is empty' do
+      expect{Noir::Command::New::GitIgnore.gitignore_texts_from_kinds([])}.to raise_error(/input/)
+    end
+
+    it 'is raise errors when arguments has invalid kinds' do
+      expect{Noir::Command::New::GitIgnore.gitignore_texts_from_kinds(['hoge'])}.to raise_error(/Un/)
+    end
+
+    it 'is same text when valid a kind' do
+      %w(Vim TeX).each do |k|
+        valid_gitignore_text = Noir::Command::New::GitIgnore::GitIgnoreTexts.const_get(k)
+        expect(Noir::Command::New::GitIgnore.gitignore_texts_from_kinds([k])).to eq(valid_gitignore_text)
+      end
+    end
+
+    it 'is support multi kinds' do
+      kinds = %w(Vim TeX Vim)
+      texts = (Noir::Command::New::GitIgnore.gitignore_texts_from_kinds(kinds))
+      kinds.each do |k|
+        expect(texts).to include(Noir::Command::New::GitIgnore::GitIgnoreTexts.const_get(k))
+      end
+    end
   end
 end
 
