8a9d62bbf3de084ace4d2f04c1166bc8da77f25d
atton <e115763@gmail.com>
2015-12-24 19:58:19 +0900

    Add argument to Noir::Command::Edit::Note

--- lib/noir/command/edit/note.rb
+++ lib/noir/command/edit/note.rb
@@ -1,5 +1,5 @@
 class Noir::Command::Edit::Note < Noir::Base::TerminalCommand
-  @description = 'Create and edit for today note.'
+  @description = 'Create and edit for note. if you set negative number in argument, edit before note.'
 
   class << self
 
@@ -8,10 +8,28 @@ class Noir::Command::Edit::Note < Noir::Base::TerminalCommand
     end
 
     def execute *args
+
+      if args.size.zero?
+        edit_today_note
+      else
+        edit_before_note args.first.to_i
+      end
+    end
+
+    def edit_today_note
       search_pattern = '*' + Time.now.strftime(Noir::Command::New::Note::TimeFormat) + '*'
       today_files    = Dir.glob(search_pattern)
       target         = today_files.sort.last || Noir::Command::New::Note.create_new_note
       edit target
+    end
+
+    def edit_before_note diff
+      notes = Dir.glob(Noir::Command::New::Note::FileNameGlob)
+      if notes.size > diff.abs.succ
+        edit notes[diff-1]
+      else
+        raise "Cannot find notes before #{diff} count."
+      end
     end
   end
 end
--- spec/noir/command/edit/note_spec.rb
+++ spec/noir/command/edit/note_spec.rb
@@ -19,6 +19,26 @@ describe 'Noir::Command::Edit::Note' do
       expect(Noir::Command::Edit::Note).to receive(:edit).with('hoge')
       Noir::Command::Edit::Note.execute
     end
+
+    it 'is edit before note if set negative number' do
+      allow(Dir).to receive(:glob).with(anything).and_return([1, 2, 3, 4, 5])
+      expect(Noir::Command::Edit::Note).to receive(:edit).with(4)
+      Noir::Command::Edit::Note.execute '-1'
+    end
+
+    it 'is edit before note if set negative number in range' do
+      allow(Dir).to receive(:glob).with(anything).and_return([1, 2, 3, 4, 5])
+      expect(Noir::Command::Edit::Note).to receive(:edit).with(2)
+      Noir::Command::Edit::Note.execute '-3'
+    end
+
+    it 'returns error if set negative number out of range' do
+      allow(Dir).to receive(:glob).with(anything).and_return([1, 2, 3, 4, 5])
+      expect(Noir::Command::Edit::Note).to_not receive(:edit)
+      [-4,-5,-6].each do |n|
+        expect{Noir::Command::Edit::Note.execute(n.to_s)}.to raise_error(/Cannot/)
+      end
+    end
   end
 
 end
