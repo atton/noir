9f3a4a78c9c0f31d8b46e1788b820984200137fa
atton <e115763@gmail.com>
2019-12-07 14:52:09 +0900

    Remove `noir summarize` command

--- lib/noir/command/summarize.rb
+++
@@ -1,5 +0,0 @@
-class Noir::Command::Summarize < Noir::Base::Command
-  @description = 'summarize utilities'
-end
-
-require 'noir/command/summarize/note'
--- lib/noir/command/summarize/note.rb
+++
@@ -1,5 +0,0 @@
-class Noir::Command::Summarize::Note < Noir::Base::Command
-  @description = 'summarize notes'
-end
-
-require 'noir/command/summarize/note/weekly'
